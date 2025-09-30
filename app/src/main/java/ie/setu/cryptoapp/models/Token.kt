dd31bfe56c8bb2e95323474143dc0cbec3cbfb56
tommyc2 <20101841@mail.wit.ie>
2025-09-30 14:36:36 +0100

    Initial layout for Add Token view
    Signed-off-by: tommyc2 <20101841@mail.wit.ie>

--- app/src/main/java/ie/setu/cryptoapp/models/Token.kt
+++ app/src/main/java/ie/setu/cryptoapp/models/Token.kt
@@ -2,9 +2,8 @@ package ie.setu.cryptoapp.models
 
 data class Token(val name: String,
                  val contractAddress: String,
-                 val iconUrl: String,
                  val price: Double,
                  val priceChange24h: Double,
                  val liquidity: Double,
                  val marketCap: Double,
-                 val totalSupply: Double)
+                 val totalSupply: Double = marketCap / price)
